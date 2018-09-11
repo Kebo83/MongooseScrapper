ff73dd397c234bb5030d37f0d278e87f66cdfcd8
Kebo <36769833+Kebo83@users.noreply.github.com>
2018-09-10 19:41:13 -0700

    update2

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
-db = heroku
+// db = heroku
 
 // Routes
 
